034b7496db38afee4f0c386baab264505da43c4b
Denny Hong <hong961127@gmail.com>
2022-02-26 21:10:25 -0800

    Finish hooks workshop 1 & 2

--- react-hooks/src/exercise/01.js
+++ react-hooks/src/exercise/01.js
@@ -3,19 +3,25 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+  // let name = ''
+  // Doesn't work because with regular variables Greeting is only getting called one time
+  // when we update name with name = "xxx", the component doesn't re-render
+
+  const [name, setName] = React.useState(initialName)
+  // With useState hook, when we call setName with a value, react re-render the component
+  // for us, in the new render, the name state's value will be the value we called setName with
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +29,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Denny" />
 }
 
 export default App
--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -3,17 +3,54 @@
 
 import * as React from 'react'
 
+// A custom hook is a JavaScript function that uses one or more Reac built-in or custom hooks
+function useLocalStorageState(
+  initialState,
+  localStorageKey,
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  console.log('rerenders')
+  const [state, setState] = React.useState(getInitialState)
+  // Although initial value passed into useState is not used for subsequent re-renders
+  // the expression is still being evaluated(ran), and that could be a performance bottleneck
+
+  // Pass a callback(lazy initializer) that returns initial state as the initial value
+  // the lazy initializer is only ran on the initial render, subsequent updates does not trigger it
+  // This is a useful technique when we need to do expensive calculation for initial state value
+  // Creating the lazy initializer itself also cost resource, only use this for heavy computations
+  function getInitialState() {
+    console.log('getInitialState run')
+    const value = localStorage.getItem(localStorageKey)
+    try {
+      if (value) return deserialize(value)
+      return typeof initialState === 'function' ? initialState() : initialState
+    } catch (error) {
+      // deserialize could throw
+      return typeof initialState === 'function' ? initialState() : initialState
+    }
+  }
+
+  const prevKeyRef = React.useRef(localStorageKey)
+  React.useEffect(() => {
+    console.log('useEffect')
+    const prevKey = prevKeyRef.current
+    if (prevKey !== localStorageKey) localStorage.removeItem(prevKey) // key changed, need to delete old key value pair
+    prevKeyRef.current = localStorageKey // update key
+    localStorage.setItem(localStorageKey, serialize(state))
+  }, [state, localStorageKey, serialize]) // objects in deps list is compared with === (identity)
+  // When deps array is omitted, the effect callback is run everytime Greeting
+  // is render/re-rendered, this includes when the parent component rerenders
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  const [name, setName] = useLocalStorageState(initialName, 'name')
 
   function handleChange(event) {
-    setName(event.target.value)
+    const newName = event.target.value
+    setName(newName)
   }
   return (
     <div>
@@ -27,7 +64,13 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  const [count, setCount] = React.useState(0)
+  return (
+    <React.Fragment>
+      <button onClick={() => setCount(prev => prev + 1)}>{count}</button>
+      <Greeting initialName="Denny" />
+    </React.Fragment>
+  )
 }
 
 export default App
