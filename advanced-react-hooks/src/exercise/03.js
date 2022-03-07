bf33c0d6ea6d9b06e870d400fa8e5c34ea5fe024
Denny Hong <hong961127@gmail.com>
2022-03-06 22:08:15 -0800

    Finish useContext section

--- advanced-react-hooks/src/exercise/03.js
+++ advanced-react-hooks/src/exercise/03.js
@@ -1,40 +1,49 @@
 // useContext: simple Counter
 // http://localhost:3000/isolated/exercise/03.js
 
-import * as React from 'react'
+import * as React from "react";
 
-// 🐨 create your CountContext here with React.createContext
-
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+const CountContext = React.createContext();
 
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
-  return <div>{`The current count is ${count}`}</div>
+  const [count] = useCount();
+  return <div>{`The current count is ${count}`}</div>;
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
-  const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>Increment count</button>
+  const [, setCount] = useCount();
+  const increment = () => setCount(c => c + 1);
+  return <button onClick={increment}>Increment count</button>;
+}
+
+function useCount() {
+  const value = React.useContext(CountContext);
+  if (!value) {
+    throw new Error(
+      "useCount must be used in a component wrapped in <CountContext.Provider/>",
+    );
+  }
+  return value;
+}
+
+function CountProvider({children}) {
+  const [count, setCount] = React.useState(0);
+  return (
+    <CountContext.Provider value={[count, setCount]}>
+      {children}
+    </CountContext.Provider>
+  );
 }
 
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
