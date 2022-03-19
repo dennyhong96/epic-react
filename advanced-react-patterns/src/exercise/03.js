39cfdad6483c3e7b892838fbf5922fcafe4e2b19
Denny Hong <hong961127@gmail.com>
2022-03-19 13:39:48 -0700

    Finish advanced pattern - Flexible Compound components

--- advanced-react-patterns/src/exercise/03.js
+++ advanced-react-patterns/src/exercise/03.js
@@ -1,61 +1,64 @@
 // Flexible Compound Components
 // http://localhost:3000/isolated/exercise/03.js
 
-import * as React from 'react'
-import {Switch} from '../switch'
+import * as React from "react";
+import {Switch} from "../switch";
 
-// 🐨 create your ToggleContext context here
-// 📜 https://reactjs.org/docs/context.html#reactcreatecontext
+const ToggleContext = React.createContext(null);
+const ToggleProvider = ({children}) => {
+	const [on, setOn] = React.useState(false);
+	const toggle = React.useCallback(() => setOn(!on), [on]);
+	return (
+		<ToggleContext.Provider value={{on, toggle}}>
+			{children}
+		</ToggleContext.Provider>
+	);
+};
+const useToggle = () => {
+	const context = React.useContext(ToggleContext);
+	if (!context) {
+		throw new Error(`useToggle must be used inside a ToggleProvider`);
+	}
+	return context;
+};
 
 function Toggle({children}) {
-  const [on, setOn] = React.useState(false)
-  const toggle = () => setOn(!on)
-
-  // 🐨 remove all this 💣 and instead return <ToggleContext.Provider> where
-  // the value is an object that has `on` and `toggle` on it.
-  return React.Children.map(children, child => {
-    return typeof child.type === 'string'
-      ? child
-      : React.cloneElement(child, {on, toggle})
-  })
+	return <ToggleProvider>{children}</ToggleProvider>;
 }
 
-// 🐨 we'll still get the children from props (as it's passed to us by the
-// developers using our component), but we'll get `on` implicitly from
-// ToggleContext now
-// 🦉 You can create a helper method to retrieve the context here. Thanks to that,
-// your context won't be exposed to the user
-// 💰 `const context = React.useContext(ToggleContext)`
-// 📜 https://reactjs.org/docs/hooks-reference.html#usecontext
-function ToggleOn({on, children}) {
-  return on ? children : null
+function ToggleOn({children}) {
+	const {on} = useToggle();
+	return on ? children : null;
 }
 
-// 🐨 do the same thing to this that you did to the ToggleOn component
-function ToggleOff({on, children}) {
-  return on ? null : children
+function ToggleOff({children}) {
+	const {on} = useToggle();
+	return on ? null : children;
 }
 
-// 🐨 get `on` and `toggle` from the ToggleContext with `useContext`
-function ToggleButton({on, toggle, ...props}) {
-  return <Switch on={on} onClick={toggle} {...props} />
+function ToggleButton({...props}) {
+	const {on, toggle} = useToggle();
+	return <Switch on={on} onClick={toggle} {...props} />;
 }
 
 function App() {
-  return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <div>
-          <ToggleButton />
-        </div>
-      </Toggle>
-    </div>
-  )
+	return (
+		<div>
+			<Toggle>
+				<ToggleOn>The button is on</ToggleOn>
+				<ToggleOff>The button is off</ToggleOff>
+				{/* cloneElement solution must have compound components as direct children */}
+				{/* with Context, we can have compound components nested however deep */}
+				<div>
+					<ToggleButton />
+				</div>
+				<span>Hello</span>
+			</Toggle>
+		</div>
+	);
 }
 
-export default App
+export default App;
 
 /*
 eslint
