ff7cf505dde5348adf91170b1f436e753112a83a
Denny Hong <hong961127@gmail.com>
2022-05-11 23:01:28 -0700

    Finish advanced pattern - Controlled Props

--- advanced-react-patterns/src/exercise/06.js
+++ advanced-react-patterns/src/exercise/06.js
@@ -1,154 +1,224 @@
 // Control Props
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
-import {Switch} from '../switch'
+import * as React from "react";
+import {Switch} from "../switch";
+import warning from "warning";
 
-const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
+const callAll =
+	(...fns) =>
+	(...args) =>
+		fns.forEach(fn => fn?.(...args));
 
 const actionTypes = {
-  toggle: 'toggle',
-  reset: 'reset',
-}
+	toggle: "toggle",
+	reset: "reset",
+};
 
 function toggleReducer(state, {type, initialState}) {
-  switch (type) {
-    case actionTypes.toggle: {
-      return {on: !state.on}
-    }
-    case actionTypes.reset: {
-      return initialState
-    }
-    default: {
-      throw new Error(`Unsupported type: ${type}`)
-    }
-  }
+	switch (type) {
+		case actionTypes.toggle: {
+			return {on: !state.on};
+		}
+		case actionTypes.reset: {
+			return initialState;
+		}
+		default: {
+			throw new Error(`Unsupported type: ${type}`);
+		}
+	}
+}
+
+function useControlledSwitchWarning(
+	controlPropValue,
+	controlPropName,
+	componentName,
+) {
+	const isControlled = ![null, undefined].includes(controlPropValue);
+	const wasControlledRef = React.useRef(isControlled);
+	const {current: wasControlled} = wasControlledRef;
+
+	React.useEffect(() => {
+		warning(
+			!(isControlled && !wasControlled),
+			`Warning: Prop ${controlPropName} is changing an uncontrolled input of type undefined to be controlled. Decide between using a controlled or uncontrolled value for the lifetime of the ${componentName} component. More info: https://fb.me/react-controlled-components`,
+		);
+		warning(
+			!(!isControlled && wasControlled),
+			`Warning: Prop ${controlPropName} is changing a controlled input of type undefined to be uncontrolled. Decide between using a controlled or uncontrolled value for the lifetime of the ${componentName} component. More info: https://fb.me/react-controlled-components`,
+		);
+	}, [isControlled, wasControlled, componentName, controlPropName]);
+}
+
+function useOnChangeReadOnlyWarning(
+	controlPropValue,
+	controlPropName,
+	componentName,
+	hasOnChange,
+	onChangePropName,
+	readOnly,
+	readOnlyPropName,
+	initialValueProp,
+) {
+	const isControlled = ![null, undefined].includes(controlPropValue);
+	React.useEffect(() => {
+		warning(
+			!(isControlled && !hasOnChange && !readOnly),
+			`${componentName} Component: Warning: Failed prop type: You provided a \`${controlPropName}\` prop to a form field without an \`onChange\` handler. This will render a read-only field. If the field should be mutable use \`defaultValue\`. Otherwise, set either \`${onChangePropName}\` or \`${readOnlyPropName}\`.`,
+		);
+	}, [
+		controlPropValue,
+		controlPropName,
+		componentName,
+		hasOnChange,
+		onChangePropName,
+		readOnly,
+		readOnlyPropName,
+		initialValueProp,
+		isControlled,
+	]);
 }
 
 function useToggle({
-  initialOn = false,
-  reducer = toggleReducer,
-  // 🐨 add an `onChange` prop.
-  // 🐨 add an `on` option here
-  // 💰 you can alias it to `controlledOn` to avoid "variable shadowing."
+	initialOn = false,
+	reducer = toggleReducer,
+	on: controlledOn,
+	onChange,
+	readOnly = false,
 } = {}) {
-  const {current: initialState} = React.useRef({on: initialOn})
-  const [state, dispatch] = React.useReducer(reducer, initialState)
-  // 🐨 determine whether on is controlled and assign that to `onIsControlled`
-  // 💰 `controlledOn != null`
+	// refs
+	const {current: initialState} = React.useRef({on: initialOn});
 
-  // 🐨 Replace the next line with assigning `on` to `controlledOn` if
-  // `onIsControlled`, otherwise, it should be `state.on`.
-  const {on} = state
+	// states
+	const [state, dispatch] = React.useReducer(reducer, initialState);
 
-  // We want to call `onChange` any time we need to make a state change, but we
-  // only want to call `dispatch` if `!onIsControlled` (otherwise we could get
-  // unnecessary renders).
-  // 🐨 To simplify things a bit, let's make a `dispatchWithOnChange` function
-  // right here. This will:
-  // 1. accept an action
-  // 2. if onIsControlled is false, call dispatch with that action
-  // 3. Then call `onChange` with our "suggested changes" and the action.
+	// Derived states
+	const isControlled = ![null, undefined].includes(controlledOn);
+	const hasOnChange = Boolean(onChange);
+	const on = isControlled ? controlledOn : state.on;
 
-  // 🦉 "Suggested changes" refers to: the changes we would make if we were
-  // managing the state ourselves. This is similar to how a controlled <input />
-  // `onChange` callback works. When your handler is called, you get an event
-  // which has information about the value input that _would_ be set to if that
-  // state were managed internally.
-  // So how do we determine our suggested changes? What code do we have to
-  // calculate the changes based on the `action` we have here? That's right!
-  // The reducer! So if we pass it the current state and the action, then it
-  // should return these "suggested changes!"
-  //
-  // 💰 Sorry if Olivia the Owl is cryptic. Here's what you need to do for that onChange call:
-  // `onChange(reducer({...state, on}, action), action)`
-  // 💰 Also note that user's don't *have* to pass an `onChange` prop (it's not required)
-  // so keep that in mind when you call it! How could you avoid calling it if it's not passed?
+	// Warn devs they are using the hook wrong
+	if (process.env.NODE_ENV !== "production") {
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		useControlledSwitchWarning(controlledOn, "on", "useToggle"); // violation of rules of hook is fine here since process.env.NODE_ENV is never dynamic
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		useOnChangeReadOnlyWarning(
+			controlledOn,
+			"on",
+			"useToggle",
+			hasOnChange,
+			"onChange",
+			readOnly,
+			"readOnly",
+			initialState,
+		);
+	}
 
-  // make these call `dispatchWithOnChange` instead
-  const toggle = () => dispatch({type: actionTypes.toggle})
-  const reset = () => dispatch({type: actionTypes.reset, initialState})
+	// Handlers
+	const toggle = () => {
+		dispatchWithOnChange({type: actionTypes.toggle});
+	};
+	const reset = () => {
+		dispatchWithOnChange({type: actionTypes.reset, initialState});
+	};
 
-  function getTogglerProps({onClick, ...props} = {}) {
-    return {
-      'aria-pressed': on,
-      onClick: callAll(onClick, toggle),
-      ...props,
-    }
-  }
+	// We want to call `onChange` any time we need to make a state change, but we
+	// only want to call `dispatch` if `!onIsControlled` (otherwise we could get
+	// unnecessary renders).
+	const dispatchWithOnChange = action => {
+		if (!isControlled) dispatch(action); // if state is controlled, no need to dispatch and cause an extra render
+		if (onChange) {
+			const nextState = reducer({...state, on}, action);
+			onChange(nextState, action); // Call on change with "Suggested changes", and the action
+		}
+	};
+	// 🦉 "Suggested changes" refers to: the changes we would make if we were
+	// managing the state ourselves. This is similar to how a controlled <input />
+	// `onChange` callback works. When your handler is called, you get an event
+	// which has information about the value input that _would_ be set to if that
+	// state were managed internally.
+	// So how do we determine our suggested changes? What code do we have to
+	// calculate the changes based on the `action` we have here? That's right!
+	// The reducer! So if we pass it the current state and the action, then it
+	// should return these "suggested changes!"
 
-  function getResetterProps({onClick, ...props} = {}) {
-    return {
-      onClick: callAll(onClick, reset),
-      ...props,
-    }
-  }
+	function getTogglerProps({onClick, ...props} = {}) {
+		return {
+			"aria-pressed": on,
+			onClick: callAll(onClick, toggle),
+			...props,
+		};
+	}
 
-  return {
-    on,
-    reset,
-    toggle,
-    getTogglerProps,
-    getResetterProps,
-  }
+	function getResetterProps({onClick, ...props} = {}) {
+		return {
+			onClick: callAll(onClick, reset),
+			...props,
+		};
+	}
+
+	return {
+		on,
+		reset,
+		toggle,
+		getTogglerProps,
+		getResetterProps,
+	};
 }
 
-function Toggle({on: controlledOn, onChange}) {
-  const {on, getTogglerProps} = useToggle({on: controlledOn, onChange})
-  const props = getTogglerProps({on})
-  return <Switch {...props} />
+function Toggle({on: controlledOn, onChange, readOnly}) {
+	const {on, getTogglerProps} = useToggle({
+		on: controlledOn,
+		onChange,
+		readOnly,
+	});
+	return <Switch {...getTogglerProps({on})} />;
 }
 
 function App() {
-  const [bothOn, setBothOn] = React.useState(false)
-  const [timesClicked, setTimesClicked] = React.useState(0)
+	const [bothOn, setBothOn] = React.useState(false);
+	const [timesClicked, setTimesClicked] = React.useState(0);
 
-  function handleToggleChange(state, action) {
-    if (action.type === actionTypes.toggle && timesClicked > 4) {
-      return
-    }
-    setBothOn(state.on)
-    setTimesClicked(c => c + 1)
-  }
+	function handleToggleChange(nextState, action) {
+		if (action.type === actionTypes.toggle && timesClicked > 4) return;
+		setBothOn(nextState.on);
+		setTimesClicked(c => c + 1);
+	}
 
-  function handleResetClick() {
-    setBothOn(false)
-    setTimesClicked(0)
-  }
+	function handleResetClick() {
+		setBothOn(false);
+		setTimesClicked(0);
+	}
 
-  return (
-    <div>
-      <div>
-        <Toggle on={bothOn} onChange={handleToggleChange} />
-        <Toggle on={bothOn} onChange={handleToggleChange} />
-      </div>
-      {timesClicked > 4 ? (
-        <div data-testid="notice">
-          Whoa, you clicked too much!
-          <br />
-        </div>
-      ) : (
-        <div data-testid="click-count">Click count: {timesClicked}</div>
-      )}
-      <button onClick={handleResetClick}>Reset</button>
-      <hr />
-      <div>
-        <div>Uncontrolled Toggle:</div>
-        <Toggle
-          onChange={(...args) =>
-            console.info('Uncontrolled Toggle onChange', ...args)
-          }
-        />
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<div>
+				<Toggle on={bothOn} readOnly />
+				<Toggle on={bothOn} onChange={handleToggleChange} />
+			</div>
+			{timesClicked > 4 ? (
+				<div data-testid="notice">
+					Whoa, you clicked too much!
+					<br />
+				</div>
+			) : (
+				<div data-testid="click-count">Click count: {timesClicked}</div>
+			)}
+			<button onClick={handleResetClick}>Reset</button>
+			<hr />
+			<div>
+				<div>Uncontrolled Toggle:</div>
+				<Toggle
+					onChange={(...args) =>
+						console.info("Uncontrolled Toggle onChange", ...args)
+					}
+				/>
+			</div>
+		</div>
+	);
 }
 
-export default App
-// we're adding the Toggle export for tests
-export {Toggle}
+export default App;
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
+// we're adding the Toggle export for tests
+export {Toggle};
--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -1,16 +1,16 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
+import * as React from "react";
 
-// A custom hook is a JavaScript function that uses one or more Reac built-in or custom hooks
+// A custom hook is a JavaScript function that uses one or more React built-in or custom hooks
 function useLocalStorageState(
   initialState,
   localStorageKey,
   {serialize = JSON.stringify, deserialize = JSON.parse} = {},
 ) {
-  console.log('rerenders')
-  const [state, setState] = React.useState(getInitialState)
+  console.log("rerenders");
+  const [state, setState] = React.useState(getInitialState);
   // Although initial value passed into useState is not used for subsequent re-renders
   // the expression is still being evaluated(ran), and that could be a performance bottleneck
 
@@ -19,38 +19,38 @@ function useLocalStorageState(
   // This is a useful technique when we need to do expensive calculation for initial state value
   // Creating the lazy initializer itself also cost resource, only use this for heavy computations
   function getInitialState() {
-    console.log('getInitialState run')
-    const value = localStorage.getItem(localStorageKey)
+    console.log("getInitialState run");
+    const value = localStorage.getItem(localStorageKey);
     try {
-      if (value) return deserialize(value)
-      return typeof initialState === 'function' ? initialState() : initialState
+      if (value) return deserialize(value);
+      return typeof initialState === "function" ? initialState() : initialState;
     } catch (error) {
       // deserialize could throw
-      return typeof initialState === 'function' ? initialState() : initialState
+      return typeof initialState === "function" ? initialState() : initialState;
     }
   }
 
-  const prevKeyRef = React.useRef(localStorageKey)
+  const prevKeyRef = React.useRef(localStorageKey);
   React.useEffect(() => {
-    console.log('useEffect')
-    const prevKey = prevKeyRef.current
-    if (prevKey !== localStorageKey) localStorage.removeItem(prevKey) // key changed, need to delete old key value pair
-    prevKeyRef.current = localStorageKey // update key
-    localStorage.setItem(localStorageKey, serialize(state))
-  }, [state, localStorageKey, serialize]) // objects in deps list is compared with === (identity)
+    console.log("useEffect");
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== localStorageKey) localStorage.removeItem(prevKey); // key changed, need to delete old key value pair
+    prevKeyRef.current = localStorageKey; // update key
+    localStorage.setItem(localStorageKey, serialize(state));
+  }, [state, localStorageKey, serialize]); // objects in deps list is compared with === (identity)
   // When deps array is omitted, the effect callback is run everytime Greeting
   // is render/re-rendered, this includes when the parent component rerenders
 
-  return [state, setState]
+  return [state, setState];
 }
 
-function Greeting({initialName = ''}) {
+function Greeting({initialName = ""}) {
   // 🐨 initialize the state to the value from localStorage
-  const [name, setName] = useLocalStorageState(initialName, 'name')
+  const [name, setName] = useLocalStorageState(initialName, "name");
 
   function handleChange(event) {
-    const newName = event.target.value
-    setName(newName)
+    const newName = event.target.value;
+    setName(newName);
   }
   return (
     <div>
@@ -58,19 +58,19 @@ function Greeting({initialName = ''}) {
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
     </div>
-  )
+  );
 }
 
 function App() {
-  const [count, setCount] = React.useState(0)
+  const [count, setCount] = React.useState(0);
   return (
     <React.Fragment>
       <button onClick={() => setCount(prev => prev + 1)}>{count}</button>
       <Greeting initialName="Denny" />
     </React.Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
